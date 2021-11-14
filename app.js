4ecb21fec5c9d7ee279dca05c009eab6b51c5394
hs674 <hs674@drexel.edu>
2021-11-13 22:11:25 -0500

    added bootstrap and login page

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send("App")
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/html/login.html');
 })
 
 app.listen(port, () => {
@@ -11,4 +11,37 @@ app.listen(port, () => {
 })
 
 
-app.use(express.static('html'))
+
+app.get('/css/style.css', function (req, res) {
+  res.sendFile(__dirname + '/html/css/style.css');
+})
+
+app.get('/js/jquery.min.js', function (req, res) {
+  res.sendFile(__dirname + '/html/js/jquery.min.js');
+})
+app.get('/js/popper.js', function (req, res) {
+  res.sendFile(__dirname + '/html/js/popper.js');
+})
+
+app.get('/js/main.js"', function (req, res) {
+  res.sendFile(__dirname + '/js/main.js"');
+})
+
+app.get('/js/bootstrap.min.js', function (req, res) {
+  res.sendFile(__dirname + '/html/js/bootstrap.min.js');
+})
+
+app.get('/images/bb.jpeg', function (req, res) {
+  res.sendFile(__dirname + '/html/images/bb.jpeg');
+})
+
+
+app.get('/js/popper.min.js.map', function (req, res) {
+  res.sendFile(__dirname + '/html/js/popper.min.js.map');
+})
+
+
+
+app.get('/js/bootstrap.min.js.map', function (req, res) {
+  res.sendFile(__dirname + '/node_modules/bootstrap/dist/js/bootstrap.min.js.map');
+})
