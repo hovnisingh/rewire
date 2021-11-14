63cceae4883de8ed7d9f0bb4847cbdcfdf37fcb1
hs674 <hs674@drexel.edu>
2021-11-13 22:56:14 -0500

    refactored code & fixed some formatting issues

--- app.js
+++ app.js
@@ -12,36 +12,7 @@ app.listen(port, () => {
 
 
 
-app.get('/css/style.css', function (req, res) {
-  res.sendFile(__dirname + '/html/css/style.css');
-})
-
-app.get('/js/jquery.min.js', function (req, res) {
-  res.sendFile(__dirname + '/html/js/jquery.min.js');
-})
-app.get('/js/popper.js', function (req, res) {
-  res.sendFile(__dirname + '/html/js/popper.js');
-})
-
-app.get('/js/main.js"', function (req, res) {
-  res.sendFile(__dirname + '/js/main.js"');
-})
-
-app.get('/js/bootstrap.min.js', function (req, res) {
-  res.sendFile(__dirname + '/html/js/bootstrap.min.js');
-})
-
 app.get('/images/bb.jpeg', function (req, res) {
   res.sendFile(__dirname + '/html/images/bb.jpeg');
 })
 
-
-app.get('/js/popper.min.js.map', function (req, res) {
-  res.sendFile(__dirname + '/html/js/popper.min.js.map');
-})
-
-
-
-app.get('/js/bootstrap.min.js.map', function (req, res) {
-  res.sendFile(__dirname + '/node_modules/bootstrap/dist/js/bootstrap.min.js.map');
-})
